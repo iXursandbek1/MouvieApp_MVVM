05fbfe18aac17a4714bba658983bce99a7f4ba56
Xursandbek <xursandbek06082001@mail.com>
2023-08-03 12:41:33 +0500

    App Created

---
+++ MouvieApp_MVVM/Controllers/MainViewConroller/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  MouvieApp_MVVM
+//
+//  Created by Xursandbek Qambaraliyev on 01/08/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Main View Controller"
+        self.view.backgroundColor = .yellow
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
