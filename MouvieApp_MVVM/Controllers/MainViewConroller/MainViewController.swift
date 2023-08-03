6cfe346198440bc4a0bde7e7d68c7693c0836196
Xursandbek <xursandbek06082001@mail.com>
2023-08-03 13:11:16 +0500

    Creating Main View Model

--- MouvieApp_MVVM/Controllers/MainViewConroller/MainViewController.swift
+++ MouvieApp_MVVM/Controllers/MainViewConroller/MainViewController.swift
@@ -8,23 +8,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    //MARK: IBoutets:
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: ViewModel:
+    var viewModel: MainViewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "Main View Controller"
+       configView()
+    }
+
+    func configView() {
+        
+        self.title = "Main View"
         self.view.backgroundColor = .yellow
+        
+        setupTableView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ MouvieApp_MVVM/ViewModel/MainViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewModel.swift
+//  MouvieApp_MVVM
+//
+//  Created by Xursandbek Qambaraliyev on 03/08/23.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+}
